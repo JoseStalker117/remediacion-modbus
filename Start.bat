013c73615fb68025f139debfc4fdd89dc700aa76
JoseStalker117 <josefriki23@gmail.com>
2025-07-05 06:31:45 -0600

    batchfile funcionando correctamente

--- Start.bat
+++ Start.bat
@@ -12,8 +12,7 @@ if %errorlevel% neq 0 (
     echo Error: No se pudo realizar el pull del repositorio
     echo Verifique su conexion a internet o la configuracion del repositorio
     timeout /t 3 /nobreak
-)
-else (
+) else (
     echo.
     echo Pull completado exitosamente
     echo.
@@ -29,16 +28,16 @@ REM Verificar si existe modbus-async.py
 if exist "modbus-async.py" (
     echo Ejecutando modbus-async.py...
     cls
-    py modbus-async.py
+    start py modbus-async.py
 ) else (
     echo Advertencia: modbus-async.py no encontrado
 )
 
 REM Verificar si existe modbus-window.py
-if exist "modbus-window.py" (
-    echo Ejecutando modbus-window.py...
-    py modbus-window.py
+if exist "testing.py" (
+    echo Ejecutando testing.py...
+    py testing.py
     echo.
 ) else (
-    echo Advertencia: modbus-window.py no encontrado
+    echo Advertencia: testing.py no encontrado
 )
